4d449f4777ad85be209f3ad7858e997ccabf6171
Hashim67 <103920996+Hashim67@users.noreply.github.com>
2024-05-04 15:23:31 +0500

    update

--- lib/Repository/home_repository.dart
+++ lib/Repository/home_repository.dart
@@ -1,5 +1,4 @@
 import 'dart:developer';
-
 import 'package:offline_database_crud/Api/Data/Network/base_api_service.dart';
 import 'package:offline_database_crud/Api/Data/Network/network_api_service.dart';
 import 'package:offline_database_crud/Api/Models/category_model.dart';
@@ -38,112 +37,11 @@ class HomeRepository {
         name,
         description
       );
-       log('Post Api Data >>> ${response}');
+      //  log('Post Api Data >>> ${response}');
       return response;
     } catch (e) {
       
       throw e;
     }
   }
-
-
-  // Future<bool> checkConnectivity() async {
-  //   var connectivityResult = await (Connectivity().checkConnectivity());
-  //   if (connectivityResult.contains(ConnectivityResult.mobile) ||
-  //       connectivityResult.contains(ConnectivityResult.wifi) ||
-  //       connectivityResult.contains(ConnectivityResult.bluetooth) ||
-  //       connectivityResult.contains(ConnectivityResult.ethernet) ||
-  //       connectivityResult.contains(ConnectivityResult.vpn) ||
-  //       connectivityResult.contains(ConnectivityResult.other)) {
-  //     return true;
-  //   } else {
-  //     return false;
-  //   }
-  // } 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import 'dart:developer';
-
-// import 'package:offline_database_crud/Api/Data/Network/base_api_service.dart';
-// import 'package:offline_database_crud/Api/Data/Network/network_api_service.dart';
-// import 'package:offline_database_crud/Api/Models/category_model.dart';
-// import 'package:offline_database_crud/Offline%20Database/sqflite_helper.dart';
-// import 'package:offline_database_crud/Res/app_url.dart';
-
-
-
-
-
-// class HomeRepository {
-//   BaseApiServices _apiServices = NetworkApiService();
-//   // Fetch Category  List /////////
-
-//  Future <CategoryModel> fechCategoryList() async {
-//     try {
-//       dynamic response = await _apiServices.getGetApiResponse(
-//         AppUrl.categoryGetDataEndPoint,
-//       );
-//       log('response getGetApiResponse movies >>> ${response}');
-//       await SQLHelper.storeCategories(response);
-//       return response = CategoryModel.fromMap(response);
-      
-//     } catch (e) {
-      
-//       throw e;
-//     }
-//   } 
-
-
-
-
-
-
-//   //// Post/Add Category  List /////////
-
-
-
-//  Future<CategoryModel> postCategoryList(String name, String description) async {
-//     try {
-//       dynamic response = await _apiServices.getPostApiResponse(
-//         AppUrl.categoryADDDataEndPoint,
-//         name,
-//         description
-//       );
-//       // log('response getGetApiResponse category >>> ${response}');
-//       return response;
-//     } catch (e) {
-    
-//       throw e;
-//     }
-//   }
-
-//   fetchCategoryListFromAPI() {} 
-
-
-
-
-
-
-
- 
-
-// }
